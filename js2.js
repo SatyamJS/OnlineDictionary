17189a4709eb3b35e84adfc0dab53c5a5f8b1a58
Satyam Gupta <iamsatyam760@gmail.com>
2021-09-21 13:46:35 +0530

    First commit

--- js2.js
+++ js2.js
@@ -1,23 +1,39 @@
-let ele=document.getElementById('textid');
-let btn=document.getElementById('searchbtn');
+const ele=document.querySelector('.searchWordBox');
+const btn=document.querySelector('.searchBtn');
+const meaningBox= document.querySelector(".mean")
+const mainBox= document.querySelector(".box")
+let text=ele.innerText
+
+ele.addEventListener("click",(e)=>{
+    e.preventDefault()
+})
+ele.addEventListener("keyup",(e)=>{
+    if(e.keyCode===13){
+        e.preventDefault();
+        btn.click();
+    }
+})
+let loader=document.querySelector(".loading")
+
 btn.addEventListener('click',search);
 function search(){
     let text=ele.value;
     const xhr=new XMLHttpRequest();
-    xhr.open('GET',`https://api.dictionaryapi.dev/api/v2/entries/en_US/${text}`,true);
+    xhr.open('GET',`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`,true);
+        loader.style.visibility="visible";
     xhr.onload=function(){
         if (this.status==200){
             let response=JSON.parse(this.response)
-            let str=`<li>${response[0].meanings[0].definitions[0].definition}</li>`
-            let ulele=document.querySelector("#response");
-            ulele.innerHTML=str; 
+            let str=`${response[0].meanings[0].definitions[0].definition}`
+            meaningBox.innerText=str;
+            document.querySelector(".loading").style.visibility="hidden";
         }
         else{
-            let ulele=document.querySelector("#response");
-            let str=`<li>No such Word exist</li>`;
-            ulele.innerHTML=str; 
+            document.querySelector(".loading").style.visibility="hidden";
+            let str=`Sorry, no such word found!`;
+            meaningBox.innerText=str;
         };
-
+        
     }
     xhr.send();
     
